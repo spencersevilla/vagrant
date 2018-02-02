4d1c4b73cb5b6a564c00fb37166e47bef8f435b4
Spencer Sevilla <sevilla@cs.washington.edu>
2018-02-02 13:34:58 -0800

    bunch of edits

---
+++ foreign_enb/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Step 0: COMPLAIN if not Ubuntu 17.10!
+
+# Step 1: configure network interfaces
+# Note: we want the eNB to be on 10.0.101.2
+# The EPC will be 10.0.101.3
+
+# Step 2: Install prereqs (ansible and python)
+sudo apt-get install -y ansible python2.7
+
+# Step 3: Run Ansible script to do the rest
+ansible-playbook -i "localhost," -c local epc_playbook.yml 
